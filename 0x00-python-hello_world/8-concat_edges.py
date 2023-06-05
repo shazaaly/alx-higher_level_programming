305db05ceb451d94e03cc06cecd12c30ee836e1c
shazaaly <shaza.aly@gmail.com>
2023-06-05 15:08:04 -0700

    substr

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[str.index("object"):str.index("language")] + "with Pyton"]
+print(str)
