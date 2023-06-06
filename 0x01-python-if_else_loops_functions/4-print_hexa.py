b7011b03ce7689dafe198c6f79c391eb8d00bd2e
shazaaly <shaza.aly@gmail.com>
2023-06-06 14:32:36 -0700

    4-print_hexa.py fix print

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 98):
-    if i < 10:
-        print("0x0{}".format(i))
-    elif i >= 10 and i < 16:
-        print("0x0{}".format(chr(i + 87)))
-    else:
-        print("0x{:02x}".format(i))
+for i in range(99)
+    print("{} = {}".format(i, hex(i))
