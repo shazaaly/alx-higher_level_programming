ffa3084cb24977142ac976f6aa647857c0fa9325
shazaaly <shaza.aly@gmail.com>
2023-06-06 15:47:12 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end="")
-        if i != 8 or j != 9:
-            print(", ", end="")
-print("\n")
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
