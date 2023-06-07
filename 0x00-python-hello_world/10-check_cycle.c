743dc3c783c9f4e82b5e528103073ecaa9129be5
shazaaly <shaza.aly@gmail.com>
2023-06-07 12:16:31 +0200

    check_cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,31 @@
 #include "lists.h"
-#include<stdio.h>
+#include <stdlib.h>
 
 int check_cycle(listint_t *list)
 {
-	list = NULL;
+	listint_t *prev = NULL;
+	listint_t *curr = NULL;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+
+	prev = list;
+	curr = list;
+
+	while (curr != NULL && prev != NULL)
+	{
+		curr = curr->next;
+		if (curr == prev)
+		{
+			return (1);
+		}
+		prev = prev->next;
+		curr = curr->next;
+
+	}
+
+	return (0);
 }
