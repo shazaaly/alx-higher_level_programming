be18ba811ca1904c3c127d59a25ef602bb6fb647
shazaaly <shaza.aly@gmail.com>
2023-06-08 20:38:54 +0200

    as

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     if args == 0:
         print("0 argument.")
     elif args == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(args))
-        for i in range(args):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+for i in range(args):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
