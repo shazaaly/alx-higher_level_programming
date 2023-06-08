f7b3241e0293c563c7028f2360196c9ee0c8bbc5
shazaaly <shaza.aly@gmail.com>
2023-06-08 19:32:09 +0200

    sys corr

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 import sys
 if __name__ == "__main__":
     args = len(sys.argv) - 1
-if args == 1:
-    print("1 argument:")
-    print("1: ", sys.argv[1])
-elif args > 1:
-    print("{} arguments:".format(len(sys.argv)))
-    for i in range(args):
-        print("{:d}:{:s}".format(i + 1, sys.argv[i]))
-
-else:
-    print("0 arguments")
+    if args == 1:
+        print("1 argument:")
+        print("1: ", sys.argv[1])
+    elif args > 1:
+        print("{} arguments:".format(len(sys.argv)))
+        for i in range(args):
+            print("{:d}:{:s}".format(i + 1, sys.argv[i]))
+    else:
+        print("0 arguments")
