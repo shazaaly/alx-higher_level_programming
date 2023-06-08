75b568db3689e91fd99de5743830ca46a9c8dd14
shazaaly <shaza.aly@gmail.com>
2023-06-08 15:59:22 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import variable_load_5
-    names = dir(variable_load_5)
-    for name in names:
-        if name == 'a':
-            value = getattr(variable_load_5, name)
-            print(value)
+    from variable_load_5 import a
+    print(a)
