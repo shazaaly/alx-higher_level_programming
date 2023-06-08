7e2212e4b720e29072d2970beeb25ae4ecb1b7ae
shazaaly <shaza.aly@gmail.com>
2023-06-08 19:35:33 +0200

    indent

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 import sys
 if __name__ == "__main__":
     args = len(sys.argv) - 1
-    if args == 1:
-        print("1 argument:")
-        print("1: ", sys.argv[1])
-    elif args > 1:
-        print("{} arguments:".format(len(sys.argv)))
-        for i in range(args):
-            print("{:d}:{:s}".format(i + 1, sys.argv[i]))
-    else:
-        print("0 arguments")
+if args == 1:
+    print("1 argument:")
+    print("1: ", sys.argv[1])
+elif args > 1:
+    print("{} arguments:".format(len(sys.argv)))
+    for i in range(args):
+        print("{:d}:{:s}".format(i + 1, sys.argv[i]))
+
+else:
+    print("0 arguments")
