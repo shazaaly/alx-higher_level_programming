159c5d0fee8fcb89f15fd29a9bda2a6c083d03db
shazaaly <shaza.aly@gmail.com>
2023-06-08 13:31:07 -0700

    sandbox

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("1 argument:")
-    print("1: ", sys.argv[0])
-elif len(sys.argv) > 1:
-    print("{} arguments:".format(len(sys.argv)))
-    for index, arg in enumerate(sys.argv):
-        print("{}:{}".format(index, sys.arg))
+if __name__ == "__main__":
+    import sys
 
-else:
-    print("0 arguments")
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("0 arguments.")
+    elif args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args))
+
+    for i in range(args):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
