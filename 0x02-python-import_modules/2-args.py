edbbfa43bc1580f6c3e1fe15447b0f62a29c2f85
shazaaly <shaza.aly@gmail.com>
2023-06-08 19:57:30 +0200

    sys.argv fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-import sys
+
 if __name__ == "__main__":
+    import sys
     args = len(sys.argv) - 1
     if args == 1:
         print("1 argument:")
-        print("1: ", sys.argv[1])
     elif args > 1:
-        print("{} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(args))
         for i in range(args):
-            print("{:d}:{:s}".format(i + 1, sys.argv[i]))
+            print("{:d}:{:s}".format(i + 1, sys.argv[i + 1]))
     else:
         print("0 arguments")
