84f140dee4f3dbc0d9cca5954bf14059c2b31ca6
shazaaly <shaza.aly@gmail.com>
2023-06-10 15:08:51 -0700

    5-str-replace

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return
+    else:
+        new_str = ""
+        for char in my_string:
+            if char != 'c' and char != 'C':
+                new_str += char
+        return (new_str)
