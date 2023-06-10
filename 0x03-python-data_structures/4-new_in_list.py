a407e6827a416e6941b64131ff664e75c033f563
shazaaly <shaza.aly@gmail.com>
2023-06-10 14:43:12 -0700

    4-copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list[:]
+    if idx > (len(my_list) - 1) or idx < 0:
+        return (copy)
+    else:
+        copy[idx] = element
+        return (copy)
