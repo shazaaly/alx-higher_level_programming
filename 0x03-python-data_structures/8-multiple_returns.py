5ab2501405e66910d616e7443f139a821d31a73b
shazaaly <shaza.aly@gmail.com>
2023-06-11 13:54:53 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return (None,)
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
