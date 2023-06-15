473726ec1c31c3bdc96a74313399a4a9274fe769
shazaaly <shaza.aly@gmail.com>
2023-06-15 17:21:09 +0000

    10-best scoredic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_score = None
     for key, val in a_dictionary.items():
-        if not val:
+        if not val or len(a_dictionary) == 0:
             return None
         elif max_score is None or max_score < val:
             max_score = val
