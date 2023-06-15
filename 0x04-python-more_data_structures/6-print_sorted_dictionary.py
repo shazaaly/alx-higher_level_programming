3da09530f3ccc2f442882d821ea11ff0aecbc13b
shazaaly <shaza.aly@gmail.com>
2023-06-15 15:58:04 +0000

    6-sorted dic

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return
     sorted_list = sorted(a_dictionary.keys())
     for key in sorted_list:
         value = a_dictionary[key]
