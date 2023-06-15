caaf57de04b504105f1417310ad438b2fd403c3a
shazaaly <shaza.aly@gmail.com>
2023-06-15 11:49:45 +0000

    2-unique_add use reduce

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-from functools import reduce
-
-
 def uniq_add(my_list=[]):
     my_set = set(my_list)
-    total = reduce(lambda x, y: x + y, my_set)
+    total = sum(my_set)
     return total
