a2de3d49eb40c0f1bc91f6b4cc8daef39c504413
shazaaly <shaza.aly@gmail.com>
2023-06-26 10:41:54 +0300

    safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    counter = 0
-    for item in my_list:
-        counter = counter + 1
+    count = 0
     try:
-        if counter <= x:
-            print("{}".format(item, end=""))
-    except ValueError:
-        print("x is greater than items count!")
-    finally:
-        return counter
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+
+    except IndexError:
+        pass
+    print()
+    return count
