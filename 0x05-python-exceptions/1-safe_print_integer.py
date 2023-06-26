daf03cd7a37e927c661d774f22c8ba67b7f288bf
shazaaly <shaza.aly@gmail.com>
2023-06-26 12:33:14 +0300

    TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
         print("{:d}".format(value), end="")
         print()
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
