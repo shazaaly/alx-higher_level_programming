5e69cd543c958f5c5dc59bf7496d944a56f139fa
shazaaly <shaza.aly@gmail.com>
2023-06-26 12:29:19 +0300

    safe_print_list_integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+
+    except (ValueError, TypeError):
+        pass
+    print()
+    return count
