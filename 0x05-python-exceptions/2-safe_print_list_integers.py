81aeab71105e9ade5596dcca8a607c9c35dbcd22
shazaaly <shaza.aly@gmail.com>
2023-06-26 12:46:23 +0300

    isinstance

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="")
-            count += 1
+            if isinstance(my_list[i], int):
+                print("{}".format(my_list[i]), end="")
+                count += 1
 
     except (ValueError, TypeError):
         pass
