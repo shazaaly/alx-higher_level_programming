2a44fb02da9e215d3611e592ba5771831625e652
shazaaly <shaza.aly@gmail.com>
2023-06-30 00:45:56 +0300

    a

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,12 +52,12 @@ class Square:
         if self.__size == 0:
             print()
         for j in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-
+            if self.__position[1] >= 0:
+                print("")
             for k in range(self.__position[0]):
                 print(" ", end="")
-        print("#" * self.__size)
+        for i in range(self.__size):
+            print("#" * self.__size)
 
 
 
