41bdbf6b03bb61e3e0da965955e500be0ef409bc
shazaaly <shaza.aly@gmail.com>
2023-06-30 00:55:15 +0300

    ifs

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,12 +39,15 @@ class Square:
         Args : value
         Raises: Type
         error or value error"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
         """prints in stdout the square with the character #
