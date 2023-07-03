202c8744f14d0a41b52c8b9ffe84b103d473735a
shazaaly <shaza.aly@gmail.com>
2023-07-03 15:07:21 +0300

    negatives

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,8 +10,20 @@ import unittest
 
 class TestMax(unittest.TestCase):
     def test_max_integer(self):
-        list = [2, 3, 4, 5, 6]
+        list1 = [2, 3, 4, 5, 6]
         expected_max = 6
-        actual_max = max_integer(list)
+        actual_max = max_integer(list1)
         self.assertEqual(actual_max, expected_max)
-        self.assertIsNotNone(max_integer(list))
+        self.assertIsNotNone(max_integer(list1))
+
+        list2 = [10]
+        expected_max = 10
+        actual_max = max_integer(list2)
+        self.assertEqual(actual_max, expected_max)
+        self.assertIsNotNone(max_integer(list2))
+
+        list3 = [-10, -5, -1, 0]
+        expected_max = 0
+        actual_max = max_integer(list3)
+        self.assertEqual(actual_max, expected_max)
+        self.assertIsNotNone(max_integer(list3))
