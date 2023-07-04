311dc01ffd88afcd3526e35da23844beec175807
shazaaly <shaza.aly@gmail.com>
2023-07-04 14:43:07 +0300

    repr

---
+++ 0x08-python-more_classes/4-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+Rectangle = __import__('4-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
