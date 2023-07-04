ec1b9c5c8120e53541bd732c7b5d682cdc2532f7
shazaaly <shaza.aly@gmail.com>
2023-07-04 19:20:01 +0300

    area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -109,4 +109,9 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area():
             return rect_1.area()
-        return max(rect_1.area(), rect_2.area())
+        if rect_1.area() > rect_2.area():
+            return rect_1.area()
+        else:
+            rect_2.area()
+
+
