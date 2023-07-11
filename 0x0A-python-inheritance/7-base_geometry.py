59860e94b5cea0c6c5006cd30b47617bb36f91a3
shazaaly <shaza.aly@gmail.com>
2023-07-11 09:17:58 +0300

    common cases tests

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module for basic class implementation
+"""
+
+
+class BaseGeometry:
+    """Basic class named BaseGeometry
+    """
+
+    def area(self):
+        """
+        Public instance method: raises exception
+        No implementation yet
+        Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        public instance method: validate value
+        Args :
+            name and value
+        Raises :
+            if value is not an integer: TypeError exception,
+            with the message <name> must be an integer
+            if value is less or equal to 0: raise a ValueError
+            exception with the message <name> must be greater than 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
