3eee4b4322746ee6f7be2c783e7a3800799a2d74
shazaaly <shaza.aly@gmail.com>
2023-07-11 08:35:12 +0300

    check type(obj) != a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,4 +19,7 @@ def inherits_from(obj, a_class):
     class that inherited (directly or indirectly), False otherwise.
     """
 
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
