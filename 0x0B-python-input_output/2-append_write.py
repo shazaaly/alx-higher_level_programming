30f9a8ba415548517322882837ad23fa9a0eb607
shazaaly <shaza.aly@gmail.com>
2023-07-12 17:33:18 +0300

    seek

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -14,6 +14,7 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, "a", encoding="utf-8") as file:
+        file.seek(0, 2)
         file.write(text)
 
     with open(filename, "r") as file:
