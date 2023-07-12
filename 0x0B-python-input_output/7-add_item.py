4413112c108f0d4ebee5ed14dc7b0741a66e6140
shazaaly <shaza.aly@gmail.com>
2023-07-12 19:00:33 +0300

    try exceot

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+
 
 import sys
 if __name__ == "__main__":
@@ -7,6 +9,9 @@ if __name__ == "__main__":
     load_from_json_file =\
         __import__("6-load_from_json_file").load_from_json_file
 
-    items = load_from_json_file("add_item.json")
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+        items.extend(sys.argv[1:])
+        save_to_json_file(items, "add_item.json")
