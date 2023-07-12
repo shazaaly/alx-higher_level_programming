eeb7ba1b494792222ee2ed12fe67c293c47ec737
shazaaly <shaza.aly@gmail.com>
2023-07-12 20:00:33 +0300

    if os.path.exists("add_item.json"):

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
+import os.path
+
 
 if __name__ == "__main__":
 
@@ -8,10 +10,11 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
+    if os.path.exists("add_item.json"):
+        try:
+            items = load_from_json_file("add_item.json")
+        except FileNotFoundError:
+            items = []
 
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
