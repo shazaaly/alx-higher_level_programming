4dbf1682e21ac93b12a1841e79836f1bd5aa3e80
shazaaly <shaza.aly@gmail.com>
2023-07-12 19:18:53 +0300

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
