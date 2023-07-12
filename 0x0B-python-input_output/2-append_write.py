baa41ae62c5e82c41b8c73f094fb9d55e4caf387
shazaaly <shaza.aly@gmail.com>
2023-07-12 17:38:47 +0300

    test

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -14,7 +14,7 @@ def append_write(filename="", text=""):
     """
 
     with open(filename, "a", encoding="utf-8") as file:
-        file.write("\n" + text)
+        file.write(text)
 
     with open(filename, "r") as file:
         content = file.read()
