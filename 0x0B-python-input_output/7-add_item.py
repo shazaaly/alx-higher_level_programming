4672942dc1a7294e50c9d2c5b8921010f6eba07d
shazaaly <shaza.aly@gmail.com>
2023-07-12 18:56:08 +0300

    7-add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+if __name__ == "__main__":
+
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file =\
+        __import__("6-load_from_json_file").load_from_json_file
+
+    items = load_from_json_file("add_item.json")
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
