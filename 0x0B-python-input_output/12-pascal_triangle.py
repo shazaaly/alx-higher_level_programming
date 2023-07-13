b15cb47ef6704637dfe4e22a5c5a4335cd1e40f1
shazaaly <shaza.aly@gmail.com>
2023-07-13 05:51:11 +0300

    col

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,9 +15,8 @@ def pascal_triangle(n):
         row_list = []
         row[0] = 1
         row[len(row) - 1] = 1
-        for el in row:
+        for col in range(row):
             if row >= 2:
-                el = row - 1[0] + row - 1[1]
-                row_list.append(el)
+                tri[row].append([row - 1][col - 1] + [row - 1][col])
 
-    tri.append(row_list)
+    return tri
