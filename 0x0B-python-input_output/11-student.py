0e89d5cb2e10d1a286b7a9942a5feb26bf407ea3
shazaaly <shaza.aly@gmail.com>
2023-07-13 04:08:06 +0300

    11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module is about a class Student that defines a student
+"""
+
+import json
+
+
+class Student:
+    """Public instance attributes: first_name
+       last_name and age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """_summary_
+        Args:
+        first_name (_type_)
+        last_name (_type_)
+        age (_type_)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance.
+        Args:
+            attrs (_type_, optional)
+        """
+        if attrs is None:
+
+            return self.__dict__
+        if all(type(attr) == str for attr in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance:
+        Args: json
+        """
+        data = json.loads(json)
+
+        for key, value in data.items():
+            setattr(self, key, value)
