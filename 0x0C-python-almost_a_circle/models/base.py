81d2a376734764cee11b75268292386dc819ece7
shazaaly <shaza.aly@gmail.com>
2023-07-15 17:54:27 +0300

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,11 +3,19 @@
 """This module is to manage id attribute in all future classes
 """
 
+import json
+
 
 class Base:
     """class to handle id attribute
     """
     __nb_objects = 0
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
     def __init__(self, id=None):
         """cass constructor
