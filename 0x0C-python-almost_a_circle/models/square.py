26711567f7481cf21b3dbfa0da07462ac0630f2e
shazaaly <shaza.aly@gmail.com>
2023-07-15 07:15:20 +0300

    12. Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,36 @@ class Square(Rectangle):
         """Constructor method"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """size setter"""
+        if type(val) != int:
+            raise TypeError("size must be an integer")
+        if val < 0:
+            raise ValueError("size must be greater than zero!")
+        self.width = val
+        self.height = val
+
+    def update(self, *args, **kwargs):
+        if args and len(args) != 0:
+            for arg in args:
+                if len(args) >= 1:
+                    self.id = args[0]
+                if len(args) >= 2:
+                    self.size = args[1]
+                if len(args) >= 3:
+                    self.x = args[2]
+                if len(args) >= 3:
+                    self.x = args[2]
+        else:
+            for k, v in kwargs.items():
+                return (k, v)
+
     def __str__(self):
         """_summary_
 
@@ -23,4 +53,4 @@ class Square(Rectangle):
             string representation [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
         return f"[Square]\
- ({self.id})  {self.x}/{self.y} - {self.width}/{self.width}"
+ ({self.id})  {self.x}/{self.y} - {self.width}"
