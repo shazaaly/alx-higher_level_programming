d795aa6de31d2078b78ccb5f6080b44c44905f7b
shazaaly <shaza.aly@gmail.com>
2023-07-16 17:23:18 +0300

    class TestSquare

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is the unittest of Square Module
+It contains a class for unitest
+"""
+
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
