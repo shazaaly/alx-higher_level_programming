849a90051d8b9f494d73c0e0e8fbae0033f5c335
shazaaly <shaza.aly@gmail.com>
2023-08-16 12:14:14 +0300

    11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,9 @@
+-- a script that lists all records with a score >= 10 in the table second_table
+-- Display both the score and the name (in this order)
+-- Records ordered by score (top first)
+
+SELECT score, name
+FROM `second_table`
+WHERE score >= 10
+ORDER BY score DESC;
+
