227cf0a923694d7c1d6d5648872da8adeb7f1692
shazaaly <shaza.aly@gmail.com>
2023-08-16 13:34:29 +0300

    16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- a script that lists all records of the table second_table
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+
+SELECT score, name
+FROM `second_table`
+ORDER BY score DESC;
+
