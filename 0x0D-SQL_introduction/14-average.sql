f6e93e5bed34a588997d9d1a2c3a4aeed1e15030
shazaaly <shaza.aly@gmail.com>
2023-08-16 12:56:33 +0300

    14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- a script that computes the score average of all records in the table second_table
+-- The result column name is  average
+
+SELECT
+AVG(score) AS `average`
+FROM `second_table`;
+
