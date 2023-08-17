6749e3adf437f8717997a7fb2d41a82315548677
shazaaly <shaza.aly@gmail.com>
2023-08-17 19:39:57 +0300

    10-inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,8 +4,8 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows AS shows
-INNER JOIN tv_show_genres AS genres
-ON shows.id = genres.show_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 ;
