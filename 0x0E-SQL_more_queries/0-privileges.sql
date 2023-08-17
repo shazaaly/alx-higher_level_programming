cde63b20e0e05cf56dfbdf2f72a5b6bf987e5475
shazaaly <shaza.aly@gmail.com>
2023-08-17 03:26:20 +0300

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- on (in localhost).
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
