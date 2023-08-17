4d33fa001f4cf0b9c9d7eafe6f1bd37570f495ee
shazaaly <shaza.aly@gmail.com>
2023-08-17 22:17:34 +0300

    14 multiple joinse.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,14 @@
+-- a script to lists all genres of the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_shows_genres
+ON tv_genres.id = tv_shows_genres.genre_id
+JOIN tv_shows
+ON tv_shows.id = tv_shows_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
+
