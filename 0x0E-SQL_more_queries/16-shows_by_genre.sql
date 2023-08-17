d031d80606462df0d7e2acf6de2ff6969dfdbe6b
shazaaly <shaza.aly@gmail.com>
2023-08-17 22:54:51 +0300

    16 left join

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- a script that lists all shows, and all genres linked to that show, from the database
+
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT  JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_shows_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+
