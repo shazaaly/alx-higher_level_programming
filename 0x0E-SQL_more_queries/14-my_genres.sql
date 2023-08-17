a20f3258c13bada824bbcd24a88d6c57a1ee4156
shazaaly <shaza.aly@gmail.com>
2023-08-17 22:19:50 +0300

    14 multiple joinse.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,12 +3,13 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres
 JOIN tv_shows_genres
 ON tv_genres.id = tv_shows_genres.genre_id
 JOIN tv_shows
 ON tv_shows.id = tv_shows_genres.show_id
 WHERE tv_shows.title = 'Dexter'
+GROUP BY name
 ORDER BY tv_genres.name ASC;
 
