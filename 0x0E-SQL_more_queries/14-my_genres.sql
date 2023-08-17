19372f50bff43f4f379acd21669d0baa4148904e
shazaaly <shaza.aly@gmail.com>
2023-08-17 22:35:36 +0300

    14 multiple joinse.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,9 +5,9 @@
 
 SELECT name
 FROM tv_genres
-JOIN tv_shows_genres
+LEFT JOIN tv_shows_genres
 ON tv_genres.id = tv_shows_genres.genre_id
-JOIN tv_shows
+LEFT JOIN tv_shows
 ON tv_shows.id = tv_shows_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 GROUP BY name
