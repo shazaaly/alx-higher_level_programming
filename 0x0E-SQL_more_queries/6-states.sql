eb399433d8f1adaaafcb512aa6bb02849025a5a3
shazaaly <shaza.aly@gmail.com>
2023-08-17 04:42:01 +0300

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- a script that creates the database hbtn_0d_usa and the table states
+-- states description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+CREATE TABLE IF NOT EXISTS `states`(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+
+);
