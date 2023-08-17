e0d192c6dc2bf00e3d8b7381f45d88511de8541a
shazaaly <shaza.aly@gmail.com>
2023-08-17 04:11:01 +0300

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,6 +4,5 @@
 -- If the user user_0d_1 already exists, your script should not fail
 
 
-CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIEDBY 'user_0d_1_pwd';
-GRANT ALL privileges ON '*' TO 'user_0d_1'@'localhost';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
