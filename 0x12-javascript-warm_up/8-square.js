b3b24c2ef3e79eeaaec95f614b0922f13dfbe78e
shazaaly <shaza.aly@gmail.com>
2023-09-11 20:39:03 +0300

    task 8 process.stdout.write

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (!size || isNaN(size)) {
+  console.log('Missing size');
+}
+if (size < 0) {
+  // do nothing
+}
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    process.stdout.write('x');
+  }
+  process.stdout.write('\n');
+}
