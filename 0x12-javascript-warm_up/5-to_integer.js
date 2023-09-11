038288f8b4614f286320e9d2b1049ba0d46b424b
shazaaly <shaza.aly@gmail.com>
2023-09-11 18:52:56 +0300

    isNan task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let converted = parseInt(process.argv[2])
+const converted = parseInt(process.argv[2]);
 if (!isNaN(converted)) {
-	console.log('My number: ' + converted);
-}else{
-	console.log('Not a number');
+  console.log('My number: ' + converted);
+} else {
+  console.log('Not a number');
 }
