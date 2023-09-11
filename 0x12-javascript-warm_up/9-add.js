e01dfb3364908bf3a9f3780dac8ac72bfa0ddec2
shazaaly <shaza.aly@gmail.com>
2023-09-11 21:05:43 +0300

    semi

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2])
-const b = parseInt(process.argv[3])
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-function add(a, b){
-	if (typeof a == 'undefined' || typeof b == 'undefined') {
-		console.log('NaN');
-		return;
-	}
-	console.log(a + b);
-
+function add (a, b) {
+  if (typeof a === 'undefined' || typeof b === 'undefined') {
+    console.log('NaN');
+    return;
+  }
+  console.log(a + b);
 }
-add(a, b)
+add(a, b);
