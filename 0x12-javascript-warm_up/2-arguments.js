602569c22ccbbcdda281ef663a546b90f3626c2c
shazaaly <shaza.aly@gmail.com>
2023-09-11 16:10:06 +0300

    args count

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
-console.log(process.argv.length - 2);
+switch (process.argv.length) {
+  case 2:
+    console.log('No argument');
+    break;
+  case 3:
+    console.log('Argument found');
+    break;
+
+  default:
+    console.log('Arguments found');
+    break;
+}
