f091f5c5d099dca5553e0dc63a298d53344dc929
shazaaly <shaza.aly@gmail.com>
2023-09-13 02:26:25 +0300

    6-sq

---
+++ twst.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function(s) {
+	let chars = new Set(s)
+	uniqusCount = chars.size
+	//calc freq of uniqus
+
+
+ };
+
+ let test= minDeletions("aaab")
+ console.log(test);
