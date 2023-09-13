725b4ceff7884edbd00eea6e92bf7e56f0456a10
shazaaly <shaza.aly@gmail.com>
2023-09-14 01:03:46 +0300

    const fs = require('fs');

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const dest = process.argv[4];
-
-const fs = require('fs');
 
 const fileAContent = fs.readFileSync(fileA.toString());
 const fileBContent = fs.readFileSync(fileB.toString());
