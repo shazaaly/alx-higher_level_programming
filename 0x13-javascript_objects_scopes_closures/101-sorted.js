9f8df8749f03c63ad65b879cf7f8d9c733f63461
shazaaly <shaza.aly@gmail.com>
2023-09-13 23:54:53 +0300

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-
 const dictCopy = dict;
 const newDict = {};
 
-for (userID in dictCopy) {
-  occurence = dictCopy[userID];
+for (const userID in dictCopy) {
+  const occurence = dictCopy[userID];
   if (!newDict[occurence]) {
     newDict[occurence] = [];
   }
