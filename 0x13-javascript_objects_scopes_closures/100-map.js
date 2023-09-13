84534ff65140caf9fa69729ef3b56449df84c228
shazaaly <shaza.aly@gmail.com>
2023-09-13 18:11:31 +0300

    print list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+
+const { list } = require('./100-data');
+
 const myList = require('./100-data').list;
 const newList = myList.map((index, el) => el * index);
+console.log(list);
 console.log(newList);
