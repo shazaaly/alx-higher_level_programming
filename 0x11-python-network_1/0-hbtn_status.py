26d3d2fac895f282c6bb8673d05c5d1a493c0bf4
shazaaly <shaza.aly@gmail.com>
2023-09-29 23:33:19 +0300

    fix comment

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# a Python script that fetches https://alx-intranet.hbtn.io/status
+""" a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
 if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         content_decode = content.decode('UTF-8')
         content_type = type(content).__name__
-        print('Body response')
+        print('Body response:')
         print(f"\t- type: {content_type}")
         print(f"\t- content: {content}")
         print(f"\t- utf8 content: {content_decode}")
