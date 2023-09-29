aa39bdbbe8369fea904457f74a404185c16e3e42
shazaaly <shaza.aly@gmail.com>
2023-09-30 02:11:01 +0300

    f

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,12 @@ displays the body of the response
 (decoded in utf-8).
 """
 
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
 
-import sys
-from urllib import request, error
-try:
-    with request.urlopen(sys.argv[1]) as res:
-        print(res.read().decode('UTF-8'))
-except error.HTTPError as er:
-    print('Error code:', er.code)
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
