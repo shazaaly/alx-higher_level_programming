13c996911045cc1ebdd1f2bac18e03dcf700db82
shazaaly <shaza.aly@gmail.com>
2023-09-29 23:34:06 +0300

    type

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         content_decode = content.decode('UTF-8')
-        content_type = type(content).__name__
+        content_type = type(content)
         print('Body response:')
         print(f"\t- type: {content_type}")
         print(f"\t- content: {content}")
