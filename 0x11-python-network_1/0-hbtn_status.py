4f3e33ac4bb88ea2d88698eed9a842a47404de17
shazaaly <shaza.aly@gmail.com>
2023-09-29 23:38:37 +0300

    format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" a Python script that fetches https://alx-intranet.hbtn.io/status"""
+""" a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
-        content_decode = content.decode('UTF-8')
-        content_type = type(content)
-        print('Body response:')
-        print(f"\t- type: {content_type}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content_decode}")
+        cont_type = type(content)
+        decoded = content.decode('UTF-8')
+        print("Body response:")
+        print("\t- type: {}".format(cont_type))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(decoded))
