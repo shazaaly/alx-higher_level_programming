52faef74000de1afcffac0b6c107a24cc1092ff6
shazaaly <shaza.aly@gmail.com>
2023-09-30 12:04:37 +0300

    5-task

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to
+the URL and displays the value of
+the variable X-Request-Id in the response header
+"""
+
+import requests
+import sys
+
+url = sys.argv[1]
+r = requests.get(url)
+print(r.text)
