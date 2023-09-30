f98cff3b58ba5f2da2fad1ff0549838fc36d190f
shazaaly <shaza.aly@gmail.com>
2023-09-30 02:20:59 -0700

    5-hbtn_header.py if name

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,9 +5,9 @@ the variable X-Request-Id in the response header
 """
 
 
-import requests
-import sys
-
-url = sys.argv[1]
-r = requests.get(url)
-print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
