0c308f5c9f5c8eae64e6164d2828d3e4633afc5d
shazaaly <shaza.aly@gmail.com>
2023-09-30 23:51:01 -0700

    4-header fix0s

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  a Bash script that takes in a URL and send header of x school 98
+#  a Bash script that takes in a URL as an argument,and displays the body of the response
 curl -sH "X-School-User-Id: 98" "${1}"
