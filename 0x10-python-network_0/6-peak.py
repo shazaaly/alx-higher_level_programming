dfcfb6d361efaa4baa572e48af48401f44f996e9
shazaaly <shaza.aly@gmail.com>
2023-10-01 12:07:58 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """a function that finds a peak in a list of unsorted integers."""
+    if len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+       list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+
+    else:
+        return find_peak(list_of_integers[mid+1:])
+
+    find_peak()
