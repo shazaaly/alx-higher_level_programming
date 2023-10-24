e540ba2ad847b65de318ae3e2e2e318d4f6a4230
shazaaly <shaza.aly@gmail.com>
2023-10-25 00:06:10 +0300

    semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
   const movie = JSON.parse(body);
-  console.log(movie.title);
+  console.log(err || movie.title);
 });
