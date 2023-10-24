fcfcf672c21b1af0f5fc66bfbbb9ea04c5e9c9ec
shazaaly <shaza.aly@gmail.com>
2023-10-24 23:49:09 +0300

    3-starwar api

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, (err, res, body) => {
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
