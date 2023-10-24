8d596eaf7ee069a54c887078500b8b75f928f061
shazaaly <shaza.aly@gmail.com>
2023-10-25 00:44:13 +0300

    4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const { log } = require('console');
+const request = require('request');
+const id = 18;
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  let count = 0;
+
+  const cont = JSON.parse(body);
+  const results = cont.results;
+  results.forEach(elem => {
+    if (elem.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  });
+  console.log(count);
+});
