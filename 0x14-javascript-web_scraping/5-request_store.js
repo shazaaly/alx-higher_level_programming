7f8908df2e1e83faa60825448a37abdc8e7646da
shazaaly <shaza.aly@gmail.com>
2023-10-25 13:50:01 +0300

    perm

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,6 +8,9 @@ const request = require('request');
 const fs = require('fs');
 
 request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
   fs.writeFile(path, body, format, (err) => {
     console.log(err || body);
   });
