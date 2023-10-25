8986ec25e862049fe5a329452bf2b6ba01b1538f
shazaaly <shaza.aly@gmail.com>
2023-10-25 23:56:47 +0300

    semi

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,9 +3,12 @@
 const request = require('request');
 const id = process.argv[2];
 
-url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
-  movies = JSON.parse(body);
+  if (err) {
+    console.log(err);
+  }
+  const movies = JSON.parse(body);
   const chars = movies.characters;
   for (let index = 0; index < chars.length; index++) {
     const element = chars[index];
