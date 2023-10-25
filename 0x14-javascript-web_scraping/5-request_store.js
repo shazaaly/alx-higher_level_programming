b952d312139c12621fdc26788d3413f187940664
shazaaly <shaza.aly@gmail.com>
2023-10-25 14:29:07 +0300

    createWriteStream

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,16 +2,16 @@
 
 const url = process.argv[2];
 const path = process.argv[3];
-const format = 'utf8';
+// const format = 'utf8'
 
 const request = require('request');
 const fs = require('fs');
+const writeStream = fs.createWriteStream(path);
 
 request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
-  fs.writeFile(path, body, format, (err) => {
-    console.log(err || body);
-  });
+  writeStream.write(body);
+  writeStream.end();
 });
